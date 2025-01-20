f7f97b71c37ce8a22212e2b4316ff8963ea454b8
Troy Diaz <troyk1718@gmail.com>
2025-01-20 13:15:33 -0800

    Implemented 1000 random points and runtime in brute_force.py.

--- brute_force.py
+++ brute_force.py
@@ -1,7 +1,9 @@
 import sys
 from itertools import combinations
 import math
-from a1_utils import read_input_from_cli, distance, write_output_to_file, generate_random_input_file, sort_pairs
+import random
+import time
+from a1_utils import read_input_from_cli, distance, write_output_to_file, generate_random_input_file, sort_pairs, read_file_to_list
 
 def brute_force_closest_pair(points: list[tuple[float, float]]) -> tuple[float, list[tuple[tuple[float, float], tuple[float, float]]]]:
     """
@@ -56,16 +58,28 @@ def brute_force_closest_pair(points: list[tuple[float, float]]) -> tuple[float, 
 
     return min_dist, closest_pairs
 
+
 if __name__ == "__main__":
     try:
         points = read_input_from_cli()
-        min_dist, closest_pairs = brute_force_closest_pair(points)
+        
+        generate_random_input_file(n = 1000, output_file = "input1000.txt", seed = 42)
+        print("Generated 1000 random points in brute_f")
 
+        points = read_file_to_list("input1000.txt")
+        print("Read 1000 points from input1000.txt")
+
+        start_time= time.time()
+        min_dist, closest_pairs = brute_force_closest_pair(points)
+        end_time = time.time()
+
+        print(f"Brute force took {end_time - start_time: 0.6f} seconds")
         print(f"Minimum Distance: {min_dist}")
         print("Closest Pairs:")
         for pair in closest_pairs:
             print(pair)
-        write_output_to_file(distance=min_dist, points=closest_pairs, output_file= 'brute_force_output.txt')
+        write_output_to_file(distance=min_dist, points=closest_pairs, output_file= 'bruteforce_output.txt')
     except Exception as e:
         print(f"Error: {e}")
         sys.exit(1)
+
