d645500eebd851aa120cc8bdfc0494113faa15c0
Andrew Brandenfels <brandena@oregonstate.edu>
2025-01-24 16:49:33 -0800

    Update enhancedDNCTestScript.py to give logs

--- enhancedDNCTestScript.py
+++ enhancedDNCTestScript.py
@@ -7,9 +7,10 @@ output = np.zeros((6, 11))
 for k in range(1, 6):
     for i in range(1, 11):
         points = read_file_to_list(f"input10^{k}-trial{i}.txt")
+        print(f"Computing for input10^{k}-trial{i}.txt")
         start_time = time.time()
         min_dist, closest_pairs = enhanced_divide_and_conquer_closest_pair(points)
         end_time = time.time()
+        print(f"Completed input10^{k}-trial{i}.txt. Writing time to file")
         output[k][i] = end_time - start_time
-
-np.savetxt("enhanced_dnc_runtime.csv", output, "%.10f", delimiter=',', newline="\n")
+        np.savetxt("enhanced_dnc_runtime.csv", output, "%.10f", delimiter=',', newline="\n")
